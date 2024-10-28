34f04328514a8786a11b7523ae5880325a277adf
BuildTools <unconfigured@null.spigotmc.org>
2024-10-28 21:15:32 +0900

    setting

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
